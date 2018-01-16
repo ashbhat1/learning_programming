f696c32dbac50b60f605835d8736b5d9f4013fbb
Ashwini <ashwinibhat1@gmail.com>
2018-01-16 00:05:57 -0800

    added autocorrelation function that better picks the fundamental frequency of a plucked string

--- software_guitar_tuner/guitar_tuner.py
+++ software_guitar_tuner/guitar_tuner.py
@@ -71,19 +71,45 @@ def find_note_freq(freqs,datafft):
     msk = freqs > 70
     maxamploc = np.where(datafft[msk]==np.max(datafft[msk]))[0]
     return freqs[msk][maxamploc[0]]
-    
 
+def calculate_autocorr(data,rate):
+    samp=float(rate)
+    a=(1/samp)*len(data)
+    t=np.linspace(0,a,len(data))
+    fdata = data.astype('float')
+    norm = (fdata-np.mean(fdata))/np.std(fdata)
+    corr = np.correlate(norm,norm,"full")
+    result = corr[corr.size/2:] #seems like the data gets reflected (don't understand why)
+    plt.plot(result);plt.show()
+    corrdiff=np.diff(result) #take the diff to look for peaks and valleys
+    pos=np.where(corrdiff>0)[0] #find only the positive slopes
+    neg=np.where(corrdiff<0)[0] #find only the negative slopes
+    posloc=np.intersect1d(pos,neg-1) #where do indexes align for diff when you slide neg location over by 1
+    idx=np.arange(len(result))
+    plt.plot(idx,result,'b.');plt.plot(idx[posloc+1],result[posloc+1],'r.');plt.show() #plots the raw signal and where the peaks are found
+    maxamploc=posloc[np.where(result[posloc+1]==np.max(result[posloc+1]))[0]][0] #find the max amplitude location --> going to convert this to freq
+    
+    #lag = int(raw_input("Please enter value"));
+    #temp = 1/(lag/float(len(fdata))*(t.max()))
+    freq0 = 1/(maxamploc/float(len(fdata))*(t.max()))
+    return freq0
+    
+raw_input("HIT ENTER WHEN YOU WANT TO START THE NEXT RECORDING")
 print "recording 1st round"
 data1,rate1 = record_audio('output.wav')
 freqs1,datafft1 = calculate_fft(data1,rate1)
 note1 = find_note_freq(freqs1,datafft1)
-print("Frequency is: "+str(note1))
+freq0_1 = calculate_autocorr(data1,rate1)
+print("Frequency is (as calculated by fft): "+str(note1))
+print("Frequency is (as calculated by autocorrelation): "+str(freq0_1))
 raw_input('HIT ENTER WHEN YOU WANT TO START THE NEXT RECORDING')
 print "recording 2nd round"
 data2,rate2 = record_audio('output.wav')
 freqs2,datafft2 = calculate_fft(data2,rate2)
 note2 = find_note_freq(freqs2,datafft2)
+freq0_2 = calculate_autocorr(data2,rate2)
 print("Frequency is: "+str(note2))
+print("Frequency is (as calculated by autocorrelation): "+str(freq0_2))
 plt.figure(1)
 plt.subplot(2,1,1)
 plt.plot(data1,'r');
